451fc8f45fa89dba76238e3852b58a01a4aef442
hjcenry <hejincheng1209@163.com>
2016-06-21 16:41:20 +0800

    add array

--- assets/script/Game.js
+++ assets/script/Game.js
@@ -2,19 +2,17 @@ cc.Class({
     extends: cc.Component,
 
     properties: {
-        // foo: {
-        //    default: null,
-        //    url: cc.Texture2D,  // optional, default is typeof default
-        //    serializable: true, // optional, default is true
-        //    visible: true,      // optional, default is true
-        //    displayName: 'Foo', // optional
-        //    readonly: false,    // optional, default is false
-        // },
-        // ...
         tilePre:{
             default:null,
             type:cc.Prefab
-        }
+        },
+        tiles:[
+            [null,null,null,null,null],
+            [null,null,null,null,null],
+            [null,null,null,null,null],
+            [null,null,null,null,null],
+            [null,null,null,null,null],
+        ]
     },
 
     // use this for initialization
@@ -23,16 +21,17 @@ cc.Class({
             for(var col = 0;col<5;col++){
                 var tile = cc.instantiate(this.tilePre);
                 this.node.addChild(tile);
-                // tile.setPosition(100+col,100+row);
-                // tile.setPosition((tile.width+10)*col-100,(tile.height+10)*row-100);
                 tile.setPosition((tile.width+10)*col-((tile.width)*5)/2,(tile.height+10)*row-((tile.height)*5)/2);
-//                tile.setPosition(-((tile.width+10)*5)/2,-((tile.height+10)*5)/2);
+                this.tiles[row][col] = tile;
             }
         }
     },
 
     // called every frame, uncomment this function to activate update callback
-    // update: function (dt) {
-
-    // },
+    update: function (dt) {
+for(var row=0;row<5;row++){
+            for(var col = 0;col<5;col++){
+            }
+}
+    }
 });
