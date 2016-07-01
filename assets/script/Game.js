459f88aa4c5ffc0ca5a8ac18be65b74e4aae85fe
hjcenry <hejincheng1209@163.com>
2016-07-02 01:39:46 +0800

    完成+1的基本逻辑

--- assets/script/Game.js
+++ assets/script/Game.js
@@ -41,7 +41,8 @@ cc.Class({
         powerBarBg:{
             default:null,
             type:cc.Node
-        }
+        },
+        maxNum:0
     },
 
     // use this for initialization
@@ -93,14 +94,15 @@ cc.Class({
                 while(true){
                     count++;
                     var arr = new Array();
+                    var scanArr = new Array();
                     if(count>10){
                         maxRandom++;
                     }
                     randomNum = Math.ceil(Math.random()*maxRandom);
-                    tile.getComponent("Tile").setNum(randomNum,true);
+                    tile.getComponent("Tile").setNum(randomNum,false,false);
                     tile.setPosition(5+(5+tile.width)*col+tile.width/2,5+(5+tile.height)*row+tile.height/2);
                     this.tiles[row][col] = tile;
-                    this.scanAround(row,col,-1,-1,randomNum,arr);
+                    this.scanAround(row,col,-1,-1,randomNum,arr,scanArr);
                     if(arr.length<3){
                         break;
                     }
@@ -110,14 +112,21 @@ cc.Class({
             }
         }
     },
-    // 扫描四个方向
+    // 核心扫描逻辑
     scanAround:function(row,col,lastRow,lastCol,num,arr,scanArr){
+        // cc.log("row:",row,",col:",col,",lastRow:",lastRow,",lastCol:",lastCol,",num:",num,",arr:",arr,",scanArr:",scanArr);
         if(this.tiles[row][col]==null){
             return;
         }
         var isClear = false;
-        if(scanArr!=undefined){
+        if(scanArr==undefined){
+            scanArr = new Array();
+        }
+        // 扫描过的节点不再扫描
+        if(scanArr.indexOf(row+"#"+col)==-1){
             scanArr.push(row+"#"+col);
+        }else{
+            return;
         }
         // 扫描上
         if(row<4&&(lastRow!=(row+1)||lastCol!=col)&&this.tiles[row+1][col]!=null){
@@ -179,30 +188,90 @@ cc.Class({
         var scanArr = new Array();
         this.scanAround(touchRow,touchCol,-1,-1,curNum,arr,scanArr);
         if(arr.length>=3){
+            var addScore = 0;
             for(var index in arr){
                 var row = arr[index].split("#")[0];
                 var col = arr[index].split("#")[1];
-                console.log("debug"+index+" pos:row-"+row+",col-"+col);
-                console.log("debug"+index+" tile:"+this.tiles[row][col].getComponent("Tile").numLabel.string);
-                console.log("debug"+index+" pos:touchRow-"+touchRow+",touchCol-"+touchCol);
+                addScore += parseInt(this.tiles[row][col].getComponent("Tile").numLabel.string*10);
                 if(row!=touchRow||col!=touchCol){
-                    // 执行移动动作                    
-                    this.tiles[row][col].getComponent("Tile").moveToArrPosition(touchRow,touchCol);
-                    // 生成新方块
-                    var tile = cc.instantiate(this.tilePre);
-                    tile.getComponent("Tile").game = this;
-                    tile.width = (this.tileBg.width-30)/5;
-                    tile.height = (this.tileBg.height-30)/5;
-                    var maxRandom = 5;
-                    var randomNum = Math.ceil(Math.random()*maxRandom);
-                    tile.getComponent("Tile").setNum(randomNum,true);
-                    tile.getComponent("Tile").newTile(row,col);
-                    this.tiles[row][col] = tile;
-                    this.tileBg.addChild(tile);
+                    // 执行销毁动作                    
+                    this.tiles[row][col].getComponent("Tile").destoryTile();
+                    this.tiles[row][col] = null;
+                }else{
+                    this.tiles[row][col].getComponent("Tile").setNum(curNum+1,false,true);
+                    this.maxNum = curNum+1>this.maxNum?curNum+1:this.maxNum;
+                }
+            }
+            // 更新分数
+            this.scoreNum.string = parseInt(this.scoreNum.string)+addScore;
+            this.scheduleOnce(function() {
+                // 0.1s后所有方块向下移动
+                this.moveAllTileDown();
+            },0.1);
+            // 能量条补充一格
+            for(var i=0;i<5;i++){
+                if(this.powers[i]==null){
+                    cc.log(i," null power");
+                    var power = cc.instantiate(this.powerPre);
+                    power.width = (this.powerBarBg.width-30)/5;
+                    power.height = this.powerBarBg.height-10;
+                    this.powerBarBg.addChild(power);
+                    power.setPosition(5+(5+power.width)*i+power.width/2,5+power.height/2);
+                    this.powers[i] = power;
+                    break;
+                }
+            };
+            return true;
+        }
+        return false;
+    },
+
+    moveAllTileDown:function(){
+        for (var col = 0; col < 5; col++) {
+            for (var row = 0; row < 5; row++) {
+                if (this.tiles[row][col] != null) {// 有方块
+                    for (var row1 = row; row1 > 0; row1--) {
+                        if (this.tiles[row1 - 1][col] == null){
+                            //如果没有向下移动
+                            this.tiles[row1 - 1][col] = this.tiles[row1][col];
+                            this.tiles[row1][col] = null;
+                            this.tiles[row1 - 1][col].getComponent("Tile").moveTo(row1 - 1, col);
+                        }
+                    }
                 }
             }
         }
-    }
+        this.scheduleOnce(function() {
+            // 生成新方块
+            for (var col = 0; col < 5; col++) {
+                for (var row = 0; row < 5; row++) {
+                    if(this.tiles[row][col]==null){
+                        var tile = cc.instantiate(this.tilePre);
+                        tile.getComponent("Tile").game = this;
+                        tile.width = (this.tileBg.width-30)/5;
+                        tile.height = (this.tileBg.height-30)/5;
+                        var maxRandom = this.maxNum;
+                        var randomNum = Math.ceil(Math.random()*maxRandom);
+                        tile.getComponent("Tile").setNum(randomNum,false,false);
+                        tile.getComponent("Tile").newTile(row,col);
+                        this.tiles[row][col] = tile;
+                        this.tileBg.addChild(tile);
+                    }
+                }
+            }
+            // 0.5s后遍历执行逻辑
+            this.scheduleOnce(function() {
+                var isSearch = false;
+                for (var col = 0; col < 5; col++) {
+                    for (var row = 0; row < 5; row++) {
+                        if(!isSearch){
+                            isSearch = this.tiles[row][col]!=null&&this.operateLogic(row,col,parseInt(this.tiles[row][col].getComponent("Tile").numLabel.string));
+                        }
+                    }
+                }
+            }, 0.5);        
+         }, 0.3);
+    },
 
     // called every frame, uncomment this function to activate update callback
     // update: function (dt) {
--- assets/script/Tile.js
+++ assets/script/Tile.js
@@ -21,38 +21,36 @@ cc.Class({
     onLoad: function () {
         var self = this;
         this.node.on(cc.Node.EventType.TOUCH_START,function(event){
-            self.setNum(parseInt(self.numLabel.string)+1,false);
+            self.setNum(parseInt(self.numLabel.string)+1,true,false);
         }, this.node);
     },
     newTile:function(row,col){
         this.node.setPosition(5+(5+this.node.width)*col+this.node.width/2,5+(5+this.node.height)*row+this.node.height/2);
         this.node.setScale(0);
         this.node.runAction(cc.scaleTo(0.1,1));
+        this.setArrPosition(row,col);
     },
-    moveToArrPosition:function(row,col){
+    moveTo:function(row,col){
         this.row = row;
         this.col = col;
-        this.node.setPosition(5+(5+this.node.width)*col+this.node.width/2,5+(5+this.node.height)*row+this.node.height/2);
-        // var action = cc.moveTo(0.1,cc.p(5+(5+this.node.width)*col+this.node.width/2,5+(5+this.node.height)*row+this.node.height/2));
-        // this.node.runAction(
-        //     cc.sequence(
-        //         action,cc.callFunc(
-        //             function(node){
-        //                 node.destroy();
-        //             },
-        //             this.node,
-        //             this.node
-        //         ),
-        //     )
-        // );
+        this.node.stopActionByTag(1);
+        var action = cc.moveTo(0.2,cc.p(5+(5+this.node.width)*col+this.node.width/2,5+(5+this.node.height)*row+this.node.height/2));
+        this.node.runAction(action);
+        action.setTag(1);
     },
-
+    destoryTile:function(){
+        var action = cc.sequence(cc.scaleTo(0.1,0),cc.callFunc(function(node){
+            node.destroy();
+        },this.node,this.node));
+        this.node.runAction(action);
+    },
     setArrPosition:function(row,col){
         this.row = row;
         this.col = col;
     },
 
-    setNum:function(num,isInit){
+    setNum:function(num,exeLogic,playEffect){
+        this.game.maxNum = num>this.game.maxNum?num:this.game.maxNum;
         this.numLabel.string = num;
         switch(num){
             case 1:
@@ -104,27 +102,33 @@ cc.Class({
                 this.node.color = new cc.color(255, 209, 145, 255);
                 break;
         }
-        // 消除逻辑
-        if(!isInit){
-            this.game.operateLogic(this.row,this.col,parseInt(this.numLabel.string));
+        // 播放特效
+        if(playEffect){
+            this.node.runAction(cc.sequence(cc.scaleTo(0.15,1.5),cc.scaleTo(0.15,1)));
         }
-        // 能量条逻辑
-        if(!isInit){
+        // 消除逻辑
+        if(exeLogic){
+            // 执行逻辑
+            var isMove = this.game.operateLogic(this.row,this.col,parseInt(this.numLabel.string));
             var powers = this.game.powers;
-            for (var i = powers.length - 1; i >= 0; i--) {
-                if(powers[i]!=null){
-                    var costBarAction = cc.sequence(cc.scaleTo(0.1,0),cc.callFunc(function(power){
-                        power.destroy();
-                    },null,powers[i]));
-                    powers[i].runAction(costBarAction);
-                    powers[i] = null;
-                    if(i==0){
-                        // Game Over
-                        cc.director.loadScene("overScene");
+            // 能量条-1
+            if(!isMove){
+                for (var i = powers.length - 1; i >= 0; i--) {
+                    if(powers[i]!=null){
+                        var costBarAction = cc.sequence(cc.scaleTo(0.1,0),cc.callFunc(function(power){
+                            power.destroy();
+                        },null,powers[i]));
+                        powers[i].runAction(costBarAction);
+                        powers[i] = null;
+                        break;
                     }
-                    break;
+                };
+                // 游戏结束逻辑判断：能量条为空
+                if(powers[0]==null){
+                    // Game Over
+                    cc.director.loadScene("overScene");
                 }
-            };
+            }
         }
     }
 
